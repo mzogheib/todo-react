c2b56a0f968b524afa11ecc96fd17976d1dc8f35
Marwan Zogheib <mzogheib@gmail.com>
2018-01-01 20:14:06 +1100

    [app] improve error handling on empty label

--- src/_app/index.js
+++ src/_app/index.js
@@ -12,18 +12,24 @@ class App extends Component {
   }
 
   handleAdd(label) {
-    if (!label) {
-      throw new Error('No label given.');
-    }
-
+    // Throw the error if no label but don't terminate the entire program.
+    // Just console log the error message.
+    try {
+      if (!label) {
+        throw new Error('No label given.');
+      } else {
         const items = this.state.items.slice();
-    items.push({
-      label: label,
-      done: false
-    });
-    this.setState({
-      items: items
-    });
+        items.push({
+          label: label,
+          done: false
+        });
+        this.setState({
+          items: items
+        });
+      }
+    } catch (e) {
+      console.debug(e.message);
+    }
   }
 
   handleToggle(index) {
