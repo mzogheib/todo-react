4401826172622c37d86d4fd9c32bb6c010d08c00
Marwan Zogheib <mzogheib@gmail.com>
2018-01-03 08:25:34 +1100

    Add custom style to delete button

--- src/item/index.js
+++ src/item/index.js
@@ -5,19 +5,23 @@ import Colors from '../_utils/colors';
 function Item(props) {
   const itemStyle = {
     backgroundColor: Colors.two
-  }
+  };
 
   const checkboxStyle = {
     backgroundColor: Colors.two,
     borderColor: Colors.one,
     color: Colors.one
-  }
+  };
+
+  const deleteStyle = {
+    color: Colors.one
+  };
 
   return (
     <div className='item__wrapper' style={itemStyle}>
       <input className='item__checkbox' style={checkboxStyle} type='checkbox' checked={props.item.done} onChange={props.onToggle} />
       <span className='item__label' key={props.index}>{props.item.label}</span>
-      <button onClick={props.onDelete}>X</button>
+      <button className='item__delete' style={deleteStyle} onClick={props.onDelete}></button>
     </div>
   );
 }
